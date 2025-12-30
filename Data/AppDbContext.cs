d3a33ba48dba7c86d4133bfb170d479a500f652c
zanostro <63290391+zanostro@users.noreply.github.com>
2025-12-30 14:52:12 +0100

    added back missing files

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using archiver.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace archiver.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Measurement> Measurements => Set<Measurement>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Measurement>()
+            .HasIndex(m => new { m.SensorId, m.Timestamp });
+
+        base.OnModelCreating(modelBuilder);
+    }
+}
